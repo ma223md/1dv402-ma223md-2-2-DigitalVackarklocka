1e4255c54923f395d9d43e5cd606dcf76eb7f51f
ma223md <ma223md@student.lnu.se>
2014-10-06 13:02:33 +0200

    Added project files for lab 2.2

---
+++ digitalVackarklocka/AlarmClock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class AlarmClock
+{
+    public AlarmClock()
+	{
+        // fält som kapslas in av egenskapen AlarmHour
+        private int _alarmHour;
+        // fält som kapslas in av egenskapen AlarmMinute
+        private int _alarmMinute;
+        // fält som kapslas in av egenskapen Hour
+        private int _hour;
+        // fält som kapslas in av egenskapen Minute
+        private int _minute;
+	}
+}
---
+++ digitalVackarklocka/digitalVackarklocka/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalVackarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try catch-satser som tar hjälp av test-metoder
+        }
+
+        // test av standardkonstruktor
+
+        // testa standardkonstruktor med 2 parametrar
+
+        // testa standardkonstruktor med 4 parametrar
+
+        // testa TickTock()-metoden
+
+        // testa att alarm indikeras
+
+        // testa egenskaper för undantag för felaktiga tider
+
+        // testa konstruktorer för undantag för felaktiga tider
+    }
+}
